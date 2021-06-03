5ed162512984766b157320708c55cdbf8abd555a
Krister Viirsaar <git@krister.ee>
2021-06-03 09:10:15 +0300

    Fix bug: character is a box not a snake.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ canvas.height = 500
 
 var playerX = 0
 setInterval(function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.fillStyle = "rgb(50, 210, 0)";
     ctx.fillRect(playerX, 50, 20, 20);
     playerX += 1
