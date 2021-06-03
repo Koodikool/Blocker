1db744fb70e236b548bddf577aa8cc0de65f9ace
Krister Viirsaar <git@krister.ee>
2021-06-03 09:00:51 +0300

    Stretch the canvas to fill the screen.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var canvas = document.querySelector('canvas')
 var ctx = canvas.getContext('2d')
+canvas.width = document.body.offsetWidth
+canvas.height = 500
 ctx.fillStyle = "rgb(50, 210, 0)";
 ctx.fillRect(50, 50, 20, 20);
