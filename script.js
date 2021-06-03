1ae30e13861fd4769d590afe6bd8b423a51ca4e8
Krister Viirsaar <git@krister.ee>
2021-06-03 09:11:42 +0300

    Make the box move down, diagonally.

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ canvas.width = document.body.offsetWidth
 canvas.height = 500
 
 var playerX = 0
+var playerY = 0
 setInterval(function () {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.fillStyle = "rgb(50, 210, 0)";
-    ctx.fillRect(playerX, 50, 20, 20);
+    ctx.fillRect(playerX, playerY, 20, 20);
     playerX += 1
+    playerY += 1
 }, 1000 / 25)
