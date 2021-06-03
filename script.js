9bc129a41950a15e58422466d989a0b5f88889ea
Krister Viirsaar <git@krister.ee>
2021-06-03 09:09:26 +0300

    Make your character move one pixel at a time

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ var ctx = canvas.getContext('2d')
 canvas.width = document.body.offsetWidth
 canvas.height = 500
 
+var playerX = 0
 setInterval(function () {
     ctx.fillStyle = "rgb(50, 210, 0)";
-    ctx.fillRect(50, 50, 20, 20);
+    ctx.fillRect(playerX, 50, 20, 20);
+    playerX += 1
 }, 1000 / 25)
