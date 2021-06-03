15c99c5b9e46ed62027670ce0ac6da699b84da44
Krister Viirsaar <git@krister.ee>
2021-06-03 09:08:37 +0300

    Move Rect drawing into a time loop, that redraws 25 times per second.

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ var canvas = document.querySelector('canvas')
 var ctx = canvas.getContext('2d')
 canvas.width = document.body.offsetWidth
 canvas.height = 500
-ctx.fillStyle = "rgb(50, 210, 0)";
-ctx.fillRect(50, 50, 20, 20);
+
+setInterval(function () {
+    ctx.fillStyle = "rgb(50, 210, 0)";
+    ctx.fillRect(50, 50, 20, 20);
+}, 1000 / 25)
